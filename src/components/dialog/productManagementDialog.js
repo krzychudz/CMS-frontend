915a642bfc0d3c591eb15e485cc32797676878dd
Krzysztof Dziedzic <34074568+krzychudz@users.noreply.github.com>
2020-12-28 12:46:01 +0100

    Clear Dialog state when on close

--- src/components/dialog/productManagementDialog.js
+++ src/components/dialog/productManagementDialog.js
@@ -1,21 +1,5 @@
-import Dialog from '@material-ui/core/Dialog';
-import DialogContent from '@material-ui/core/DialogContent';
-import Button from '@material-ui/core/Button';
-import Slide from '@material-ui/core/Slide';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import FormControl from '@material-ui/core/FormControl';
-import Box from '@material-ui/core/Box';
-import TextField from '@material-ui/core/TextField';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import IconButton from '@material-ui/core/IconButton';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Grid from '@material-ui/core/Grid';
-import Switch from '@material-ui/core/Switch';
-
-import CloseIcon from '@material-ui/icons/Close';
-import Image from '@material-ui/icons/Image';
+import { Dialog, DialogContent, Button, Slide, AppBar, Toolbar, Typography, FormControl, Box, TextField, CircularProgress, IconButton, FormControlLabel, Grid, Switch } from '@material-ui/core';
+import { Close as CloseIcon, Image } from '@material-ui/icons';
 
 import { useForm, Controller } from 'react-hook-form';
 import { makeStyles } from '@material-ui/core/styles';
@@ -73,6 +57,16 @@ function ProductManagementDialog(props) {
     const [imageUrl, setImageUrl] = useState(null);
     const [isInProgress, setInProgress] = useState(false);
 
+    const clearDialogData = () => {
+        setImageUrl(null);
+        setIsPublished(false);
+    }
+
+    const clearAndClose = () => {
+        clearDialogData();
+        props.handleClose();
+    }
+
     const onCreateProductClicked = async data => {
         try {
             setInProgress(true);
@@ -83,6 +77,8 @@ function ProductManagementDialog(props) {
                 isPublished: isPublished,
                 imageUrl: imageUrl
             });
+
+            clearDialogData();
             props.onProductAdded(response.data);
             props.handleClose();
         } catch (e) {
@@ -129,13 +125,13 @@ function ProductManagementDialog(props) {
     return (
         <Dialog
             open={props.open}
-            onClose={props.handleClose}
+            onClose={clearAndClose}
             fullScreen
             TransitionComponent={Transition}>
 
             <AppBar className={classes.appBar} color="primary">
                 <Toolbar>
-                    <IconButton edge="start" color="inherit" onClick={props.handleClose} aria-label="close">
+                    <IconButton edge="start" color="inherit" onClick={clearAndClose} aria-label="close">
                         <CloseIcon />
                     </IconButton>
                     <Typography variant="h6" className={classes.title}>
